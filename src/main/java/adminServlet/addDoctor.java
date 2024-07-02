aa06192c585f8357a9edd78f177d1159fb2bd99d
KuzeyErturk <kuzeyerturk04@gmail.com>
2024-07-02 15:22:59 +0300

    Create addDoctor.java

---
+++ src/main/java/adminServlet/addDoctor.java
@@ -0,0 +1,44 @@
+package adminServlet;
+
+import entity.Doctor;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author kuzeyerturk
+ */
+@WebServlet(name = "addDoctor", urlPatterns = {"/addDoctor"})
+public class addDoctor extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String department = request.getParameter("department");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("examplePU");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Doctor doctor = new Doctor();
+        doctor.setDoctorFullName(name);
+        doctor.setDoctorDepartment(department);
+        em.persist(doctor);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+
+        request.getRequestDispatcher("showDoctors").forward(request, response);
+    }
+}
+
