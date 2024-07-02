f0077fd079cb9483595aaf69085729b699e663ad
KuzeyErturk <kuzeyerturk04@gmail.com>
2024-07-02 15:23:07 +0300

    Create showPatient.java

---
+++ src/main/java/adminServlet/showPatient.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package adminServlet;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author kuzeyerturk
+ */
+@WebServlet(name = "showPatient", urlPatterns = {"/showPatient"})
+public class showPatient extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("examplePU");
+        EntityManager em = emf.createEntityManager();
+
+        Query query = em.createQuery("SELECT p FROM Patient p");
+        List<?> patients = query.getResultList();
+
+        request.setAttribute("patients", patients);
+        request.getRequestDispatcher("showPatient.jsp").forward(request, response);
+
+        em.close();
+        emf.close();
+    }
+}
