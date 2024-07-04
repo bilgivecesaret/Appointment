5363ec36a4ecb3ad5b649cd6d80b49eeb8993743
KuzeyErturk <kuzeyerturk04@gmail.com>
2024-07-04 09:33:34 +0300

    Update showDoctors.java

--- src/main/java/adminServlet/showDoctors.java
+++ src/main/java/adminServlet/showDoctors.java
@@ -28,7 +28,7 @@ public class showDoctors extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("examplePU");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my_persistence_unit");
         EntityManager em = emf.createEntityManager();
         
         Query query = em.createQuery("SELECT d FROM Doctor d");
