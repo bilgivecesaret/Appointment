db3bc9065cfda84fc6b50ea7cfd2b4312b241e53
KuzeyErturk <kuzeyerturk04@gmail.com>
2024-07-04 09:42:11 +0300

    Create adminLogout.java

---
+++ src/main/java/adminServlet/adminLogout.java
@@ -0,0 +1,22 @@
+package adminServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "adminLogout", urlPatterns = {"/adminLogout"})
+public class adminLogout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("admin");
+        response.sendRedirect("http://localhost:8080/Appointment/adminLogin.jsp");
+        
+    }
+}
