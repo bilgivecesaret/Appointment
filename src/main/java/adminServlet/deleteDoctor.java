c6e053235d3aa4a9bdc8fa8713b3c53c7857c5de
KuzeyErturk <kuzeyerturk04@gmail.com>
2024-07-04 09:50:05 +0300

    Update deleteDoctor.java

--- src/main/java/adminServlet/deleteDoctor.java
+++ src/main/java/adminServlet/deleteDoctor.java
@@ -41,6 +41,6 @@ public class deleteDoctor extends HttpServlet {
         em.close();
         emf.close();
 
-        request.getRequestDispatcher("showDoctors").forward(request, response);
+        response.sendRedirect("http://localhost:8080/Appointment/showDoctors.jsp");
     }
 }
